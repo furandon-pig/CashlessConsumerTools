666746c34bf70dc5b0879e2951dd777270264f42
furandon-pig <furandon.pig@gmail.com>
2020-08-28 00:03:44 +0900

    Jarファイルのバージョンを更新しました。

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ curl -L -O https://cashless.go.jp/assets/doc/kameiten_touroku_list.pdf
 [ ! -d lib ] && mkdir lib
 
 # PDFBoxをダウンロードする。
-[ ! -f lib/pdfbox-2.0.16.jar  ] && curl -L -o lib/pdfbox-2.0.16.jar  http://ftp.kddilabs.jp/infosystems/apache/pdfbox/2.0.16/pdfbox-2.0.16.jar
-[ ! -f lib/fontbox-2.0.16.jar ] && curl -L -o lib/fontbox-2.0.16.jar http://ftp.jaist.ac.jp/pub/apache/pdfbox/2.0.16/fontbox-2.0.16.jar
+[ ! -f lib/pdfbox-2.0.21.jar  ] && curl -L -o lib/pdfbox-2.0.21.jar  http://ftp.kddilabs.jp/infosystems/apache/pdfbox/2.0.21/pdfbox-2.0.21.jar
+[ ! -f lib/fontbox-2.0.21.jar ] && curl -L -o lib/fontbox-2.0.21.jar http://ftp.jaist.ac.jp/pub/apache/pdfbox/2.0.21/pdfbox-2.0.21.jar
 
 # PDFBoxはApache Commonsも必要とするのでこちらもダウンロードする。
 # tarで固められた状態で配布されているので、必要なJarファイルのみlibディレクトリにコピーする。
@@ -18,7 +18,7 @@ if [ ! -f libs/commons-logging-1.2.jar ]; then
   curl -L -O http://ftp.riken.jp/net/apache//commons/logging/binaries/commons-logging-1.2-bin.tar.gz
   tar zxvf commons-logging-1.2-bin.tar.gz commons-logging-1.2/commons-logging-1.2.jar
   mv commons-logging-1.2/commons-logging-1.2.jar ../lib
-  popd
+  cd ..
 fi
 
 # コンパイル後の*.classを出力するディレクトリを作成。
